222f42201d6b6ac7b6f1d02550e32618a9ed054d
connor.schweighoefer@adesso.de <connor.schweighoefer@adesso.de>
2024-10-23 10:10:54 +0200

    Fix gcd

--- Hausaufgaben/funktionen/gcd.c
+++ Hausaufgaben/funktionen/gcd.c
@@ -6,21 +6,21 @@ int gcd(int a, int b) {
         return b;
     }
 
-    gcd(b, a % b);
+    return gcd(b, a % b);
 }
 
-void gcd_iterative(int a, int b) {
+int gcd_iterative(int a, int b) {
     while (a % b != 0) {
         int swap = a;
         a = b;
         b = swap % b;
     }
 
-    printf("%d\n", b);
+    return b;
 }
 
 int main(void) {
-    gcd_iterative(180, 18);
+    printf("%d\n", gcd_iterative(180, 18));
     printf("%d\n", gcd(180, 18));
     return 0;
 }
