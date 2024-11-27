c53bdb9187bab92b1cbc4c5675563c3d7c6a77e5
Connor Schweighoefer <squidxtv@gmail.com>
2024-11-27 11:20:59 +0100

    Add List and Stack functionality

--- main.c
+++ main.c
@@ -1,139 +1,283 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void strPrint(const char *str) {
-    printf("%s\n", str);
+struct _List {
+    int id;
+    struct _List* next;
+};
+
+typedef struct _List List;
+
+List* createList() {
+    List* list = (List*) malloc(sizeof(List));
+    list->id = 0;
+    list->next = NULL;
+    return list;
 }
 
-int strLen(const char *str) {
-    int i = 0;
-    while (*(str++) != '\0') {
-        i++;
+// 0 = not empty
+// 1 = empty
+int isEmpty(List* list) {
+    if (list->next == NULL) {
+        return 1;
     }
-    return i;
+
+    return 0;
 }
 
-void strRevert(char *str) {
-    const int length = strLen(str);
-    char *end = str + length - 1;
-
-    for (int i = 0; i < length / 2; ++i) {
-        char temp = *str;
-        *str = *end;
-        *end = temp;
-
-        str++;
-        end--;
-    }
-}
-
-int strCountVocals(const char *str) {
-    int count = 0;
-    char c;
-    while ((c = *str++) != '\0') {
-        switch (c) {
-            case 'a':
-            case 'e':
-            case 'i':
-            case 'o':
-            case 'u':
-            case 'A':
-            case 'E':
-            case 'I':
-            case 'O':
-            case 'U':
-                count++;
-        }
-    }
-    return count;
-}
-
-void swapCases(char *str) {
-    char c;
-    while ((c = *str) != '\0') {
-        if (c >= 'A' && c <= 'Z') {
-            *str = c + 32;
-        } else if (c >= 'a' && c <= 'z') {
-            *str = c - 32;
+List* findWithId(List* list, const int id) {
+    List* current = list;
+    while (current != NULL) {
+        if (current->id == id) {
+            return current;
         }
 
-        str++;
+        current = current->next;
+    }
+
+    return NULL;
+}
+
+List* findLastElement(List* list) {
+    List* current = list;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    return current;
+}
+
+void addToBegin(List* list, const int id) {
+    List* currentBegin = list->next;
+
+    List* newElement = (List*) malloc(sizeof(List));
+    newElement->id = id;
+    newElement->next = currentBegin;
+
+    list->next = newElement;
+}
+
+void addToBeginIfUnique(List* list, const int id) {
+    List* found = findWithId(list, id);
+
+    if (found != NULL) {
+        return;
+    }
+
+    addToBegin(list, id);
+}
+
+void addToEnd(List* list, const int id) {
+    List* end = findLastElement(list);
+
+    List* newElement = (List*) malloc(sizeof(List));
+    newElement->id = id;
+    newElement->next = NULL;
+
+    end->next = newElement;
+}
+
+void addToEndIfUnique(List* list, const int id) {
+    List* found = findWithId(list, id);
+
+    if (found != NULL) {
+        return;
+    }
+
+    addToEnd(list, id);
+}
+
+void printElement(List* list) {
+    printf("ID: %d\n", list->id);
+}
+
+void printList(List* list) {
+    if (isEmpty(list)) {
+        printf("List is empty.\n");
+        return;
+    }
+
+    List* current = list->next;
+    printf("List:\n");
+    while (current != NULL) {
+        printElement(current);
+        current = current->next;
     }
 }
 
-void concat(const char *s1, const char *s2, char *target) {
-    char c;
-    while ((c = *s1++) != '\0') {
-        *target++ = c;
+List* findSmallestElement(List* list) {
+    List* current = list->next;
+    List* smallest = current;
+
+    while (current != NULL) {
+        if (current->id < smallest->id) {
+            smallest = current;
+        }
+        current = current->next;
     }
 
-    while ((c = *s2++) != '\0') {
-        *target++ = c;
-    }
-
-    *target = '\0';
+    return smallest;
 }
 
-int countSubstring(char *input, char *substring) {
-    int count = 0;
+List* findLargestElement(List* list) {
+    List* current = list->next;
+    List* largest = current;
 
-    while (*input != '\0') {
-        int add = 1;
+    while (current != NULL) {
+        if (current->id > largest->id) {
+            largest = current;
+        }
+        current = current->next;
+    }
 
-        char *current = input;
-        char *substringCurrent = substring;
-        while (*substringCurrent != '\0') {
-            if (*current == '\0' || *current != *substringCurrent) {
-                add = 0;
-                break;
-            }
+    return largest;
+}
 
-            current++;
-            substringCurrent++;
+void deleteElement(List* list, List* element) {
+    List* current = list;
+    while (current != NULL) {
+        if (current->next == element) {
+            current->next = current->next->next;
+            return;
+        }
+        current = current->next;
+    }
+}
+
+void deleteFirstWithId(List* list, const int id) {
+    List* current = list;
+    while (current != NULL) {
+        if (current->next->id == id) {
+            List* toFree = current->next;
+            current->next = toFree->next;
+            free(toFree);
+            return;
         }
 
-        count += add;
-        input++;
+        current = current->next;
     }
-
-    return count;
 }
 
-int countSubstringReversed(char *input, char *substring) {
-    int count = 0;
-    const char *first = input;
+List* sortedAscList(List* list) {
+    List* result = createList();
+    List* current = result;
 
-    while (*input != '\0') {
-        int add = 1;
+    while (!isEmpty(list)) {
+        List* smallest = findSmallestElement(list);
+        deleteElement(list, smallest);
+        current->next = smallest;
+        current = current->next;
+    }
 
-        char *current = input;
-        char *substringCurrent = substring;
-        while (*substringCurrent != '\0') {
-            if (*current != *substringCurrent || (current == first && *(substringCurrent + 1) != '\0')) {
-                add = 0;
-                break;
-            }
+    return result;
+}
 
-            current--;
-            substringCurrent++;
+List* sortedDescList(List* list) {
+    List* result = createList();
+    List* current = result;
+
+    while (!isEmpty(list)) {
+        List* smallest = findLargestElement(list);
+        deleteElement(list, smallest);
+        current->next = smallest;
+        current = current->next;
+    }
+
+    return result;
+}
+
+List* findElementBefore(List* list, List* target) {
+    List* current = list;
+
+    while (current != NULL) {
+        if (current->next == target) {
+            return current;
         }
 
-        count += add;
-        input++;
+        current = current->next;
     }
 
-    return count;
+    return NULL;
 }
+
+void swapIds(List* list, int firstId, int secondId) {
+    List* first = findWithId(list, firstId);
+    List* second = findWithId(list, secondId);
+
+    List* preFirst = findElementBefore(list, first);
+    List* preSecond = findElementBefore(list, second);
+
+    preFirst->next = second;
+
+    List* temp = second->next;
+    second->next = first->next;
+
+    preSecond->next = first;
+    first->next = temp;
+}
+
+void pushStack(List* list, int id) {
+    addToEnd(list, id);
+}
+
+List* popStack(List* list) {
+    List* current = list->next;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+
+    deleteElement(list, current);
+    return current;
+}
+
+// 0 - not empty
+// 1 - empty
+int isStackEmpty(List* list) {
+    return isEmpty(list);
+}
+
 
 int main(void) {
-    char *input = "HaLLoLLooLo";
-    char substrings[4][4] = {"LL", "Lo", "LLo", "LoL"};
-    printf("Input: %s\n", input);
-    for (int i = 0; i < 4; ++i) {
-        printf("Substring[%d]: %s found %d times.\n", i, substrings[i], countSubstring(input, substrings[i]));
-    }
+    List* list = createList();
+    addToEnd(list, 1);
+    addToEnd(list, 2);
+    addToBegin(list, 3);
+    printList(list);
 
-    input = "HaLLoLLoLLLooo";
-    char substring[] = "oLL";
-    printf("Substring reversed: %s found %d times.\n", substring, countSubstringReversed(input, substring));
+    List* smallest = findSmallestElement(list);
+    printf("Smallest:\n");
+    printElement(smallest);
+
+    // deleteFirstWithId(list, 1);
+    // printList(list);
+
+    List* ascending = sortedAscList(list);
+    printList(ascending);
+    printList(list);
+
+    list = ascending;
+
+    List* largest = findLargestElement(list);
+    printf("Largest:\n");
+    printElement(largest);
+
+    List* descending = sortedDescList(list);
+    printList(descending);
+    printList(list);
+
+    list = descending;
+
+    addToBeginIfUnique(list, 1);
+    addToEndIfUnique(list, 1);
+    printList(list);
+
+    List* result = findElementBefore(list, findSmallestElement(list));
+    printf("The element before the smallest is:\n");
+    printElement(result);
+
+    swapIds(list, 3, 1);
+    printList(list);
+    swapIds(list, 3, 1);
+    printList(list);
+
     return 0;
 }
